8f0854bd5d71f5cbcc14ace3bf38e6f8b9d0176e
FixbroYT <fixbro2011@gmail.com>
2025-04-14 22:01:30 +0200

    code organize

--- api_endpoints.py
+++ api_endpoints.py
@@ -17,17 +17,20 @@ async def get_internal_user_id(tg_id: int)-> int:
         raise HTTPException(status_code=404, detail="User not found")
     return user_id
 
+# users requests
+@router.post("/users/create")
+async def create_user_endpoint(data: UserCreateRequest):
+    successful_creation = await users_requests.add_user(data.tg_id)
+    user_id = await users_requests.get_user_id(data.tg_id)
+    if not user_id or not successful_creation:
+        raise HTTPException(status_code=400, detail="User already exists or creation failed")
+
+    user = await users_requests.get_user(user_id)
+    return user
 
 @router.get("/users/{tg_id}")
 async def get_user(user_id: int = Depends(get_internal_user_id)):
     response = await users_requests.get_user(user_id)
-    return response
-
-@router.get("/upgrades")
-async def get_upgrades():
-    response = await upgrades_requests.get_all_upgrades()
-    if not response:
-        raise HTTPException(status_code=404, detail="Upgrades not found")
     return response
 
 @router.post("/users/{tg_id}/buy_upgrade/{upgrade_id}")
@@ -35,13 +38,6 @@ async def buy_upgrade(upgrade_id: int, user_id: int = Depends(get_internal_user_
     response = await upgrades_requests.buy_upgrade(user_id, upgrade_id)
     if not response:
         raise HTTPException(status_code=400, detail="Upgrade not found or not enough coins")
-    return response
-
-@router.get("/locations")
-async def get_locations():
-    response = await location_requests.get_all_locations()
-    if not response:
-        raise HTTPException(status_code=404, detail="Locations not found")
     return response
 
 @router.post("/users/{tg_id}/buy_location/{location_id}")
@@ -60,16 +56,6 @@ async def set_location(location_id: int, user_id: int = Depends(get_internal_use
 
     return response
 
-@router.post("/users/create")
-async def create_user_endpoint(data: UserCreateRequest):
-    successful_creation = await users_requests.add_user(data.tg_id)
-    user_id = await users_requests.get_user_id(data.tg_id)
-    if not user_id or not successful_creation:
-        raise HTTPException(status_code=400, detail="User already exists or creation failed")
-
-    user = await users_requests.get_user(user_id)
-    return user
-
 @router.post("/users/{tg_id}/balance/add")
 async def add_balance(tg_id: int):
     user_id = await users_requests.get_user_id(tg_id)
@@ -83,3 +69,23 @@ async def add_balance(tg_id: int):
 async def get_user_income(user_id: int = Depends(get_internal_user_id)):
     income = await users_requests.get_user_income(user_id)
     return {"income": income}
+
+
+
+# upgrades requests
+@router.get("/upgrades")
+async def get_upgrades():
+    response = await upgrades_requests.get_all_upgrades()
+    if not response:
+        raise HTTPException(status_code=404, detail="Upgrades not found")
+    return response
+
+
+
+# locations requests
+@router.get("/locations")
+async def get_locations():
+    response = await location_requests.get_all_locations()
+    if not response:
+        raise HTTPException(status_code=404, detail="Locations not found")
+    return response
